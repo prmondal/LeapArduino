fbcfef33dcecfbabe93edef21455169c27939d46
Bumba <prasenjuetce@gmail.com>
2017-09-12 23:49:00 +0530

    updated

--- toggle-led.js
+++ toggle-led.js
@@ -2,9 +2,11 @@ var Leap = require('leapjs');
 var five = require("johnny-five");
 
 var ledPin = 13;
+var gestureCheckDelay = 1000;
+var lastGestureCheckTime = 0;
 
 var board = new five.Board({
-	port: "COM17"
+	port: process.argv[2]
 });
 
 var leapControllerOptions = {
@@ -12,35 +14,41 @@ var leapControllerOptions = {
 };
 
 board.on("ready", function() {
-	console.log('Welcome to awesome world!');
+	console.log('Welcome to magical world :)');
 
 	var led = new five.Led(ledPin);
 
 	//leap controller loops to check for frames when browser is ready to draw
   	var controller = Leap.loop(leapControllerOptions, function(frame){
-	  //turn on LED if single hand is detected
-	  //(frame.hands.length == 1) ? led.on() : led.off();
+	  var currentTime = new Date().getTime();
+	  
+	  if(currentTime - lastGestureCheckTime > gestureCheckDelay) {
+		  lastGestureCheckTime = currentTime;
+		  
+		  //turn on LED if single hand is detected
+		  //(frame.hands.length == 1) ? led.on() : led.off();
 
-	  //turn on LED if circular gesture is detected
-	  //turn off LED if swipe gesture is detected
-	  if(frame.gestures.length > 0) {
-	  	frame.gestures.forEach(function(g) {
-	  		switch(g.type) {
-	  			case 'circle':
-	  				console.log('circle gesture detected.')
-	  				led.on();
-	  				break;
+		  //turn on LED if circular gesture is detected
+		  //turn off LED if swipe gesture is detected
+		  if(frame.gestures.length > 0) {
+			frame.gestures.forEach(function(g) {
+				switch(g.type) {
+					case 'circle':
+						console.log('circle gesture detected.')
+						led.on();
+						break;
 
-	  			case 'swipe':
-	  				console.log('swipe gesture detected.')
-	  				led.off();
-	  				break;
+					case 'swipe':
+						console.log('swipe gesture detected.')
+						led.off();
+						break;
 
-	  			default:
-	  				console.log('no gesture detected.')
-	  				break;
-	  		}
-	  	});
+					default:
+						console.log('no gesture detected.')
+						break;
+				}
+			});
+		  }
 	  }
 	});
 });
